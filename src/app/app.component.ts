e11d557c4c8dc2d236bff087561e8f9c50af41c1
David Tremmel <tremmel.david@hotmail.com>
2023-02-15 20:54:24 +0100

    add primeNG and bootstrap style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    // Enable ripple effect for all components
+    this.primengConfig.ripple = true;
+  }
+
   title = 'country-gdp-frontend';
 }
